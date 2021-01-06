66502e0e79583993df65dfe57dcec21daa24bb03
Ramune6110 <Arduino6110@gmail.com>
2021-01-06 15:34:55 +0900

    [add]First Commit

---
+++ AtCoder_ProblemB.cpp
@@ -0,0 +1,25 @@
+// ABC042B
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<stdlib.h>
+using namespace std;
+ 
+int main()
+{
+    string s[101];
+    int a, b;
+    cin >> a >> b;
+    for(int i = 0; i < a; i++)
+    {
+        cin >> s[i];
+    }
+    sort(s, s+a);
+    for(int i = 0; i < a; i++)
+    {
+        cout << s[i];
+    }
+
+    return 0;
+}
+
