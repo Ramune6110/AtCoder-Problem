0da2a6a126ad84e8e2cab19d1188ee60fa4f2b9d
Ramune6110 <Arduino6110@gmail.com>
2021-01-07 12:08:52 +0900

    [add]ProblemA

--- AtCoder_ProblemA.cpp
+++ AtCoder_ProblemA.cpp
@@ -29,3 +29,128 @@ int main()
 
     return 0;
 }
+
+// ABC043A
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    int ans = 0;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        ans += i;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
+// ABC044A
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int N, K, X, Y;
+    int sum = 0;
+
+    cin >> N >> K >> X >> Y;
+
+    for (int i = 1; i <= N; i++) {
+        if (i <= K) {
+            sum += X;
+        } else if (i >= K + 1) {
+            sum += Y;
+        }
+    }
+
+    cout << sum  << endl;
+
+    return 0;
+}
+
+// ABC045A
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int a, b, h;
+
+    cin >> a >> b >> h;
+
+    cout << (a + b) * h / 2 << endl;
+
+    return 0;
+}
+
+// ABC046A
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int ans[3], count;
+
+    for (int i = 0; i < 3; i++) {
+        cin >> ans[i];
+    }
+
+    if ((ans[0] == ans[1]) && (ans[1] == ans[2]) && (ans[0] == ans[2])) 
+    {
+        count = 1;
+    } 
+    else if ((ans[0] == ans[1]) || (ans[1] == ans[2]) || (ans[0] == ans[2]))
+    {
+        count = 2;
+    }
+    else 
+    {
+        count = 3;
+    }
+
+    cout << count;
+
+    return 0;
+}
