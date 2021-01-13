aa2d7ecb9a61966961f3e63c2c64b4435d976c08
Ramune6110 <arduino6110@gmail.com>
2021-01-13 17:14:43 +0900

    [modify]test code

--- code.cpp
+++ code.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <set>
+
 using namespace std;
 
-int main() {
-  cout << "こんにちは" << endl;
-  cout << "AtCoder" << endl;
+int main()
+{
+    int A[3];
+
+    for (int i = 0; i < 3; i++) {
+        cin >> A[i];
+    }
+
+    sort(A, A + 3);
+
+    if ((A[0] == 5) && (A[1] == 5) && (A[2] == 7)) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
 }
