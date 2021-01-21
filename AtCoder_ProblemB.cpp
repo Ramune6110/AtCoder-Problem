a5e087f66e1030d7fc41494163af9ac9f79264b6
Ramune6110 <arduino6110@gmail.com>
2021-01-21 10:12:08 +0900

    [add]Problem 054B

--- AtCoder_ProblemB.cpp
+++ AtCoder_ProblemB.cpp
@@ -419,3 +419,70 @@ int main()
 
     return 0;
 }
+
+// ABC054B
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    char A[50][50], B[50][50];
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    bool flag = false;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if ((i + M -1 >= N) || (j + M - 1 >= N)) {
+                continue;
+            }
+
+            bool match = true;
+            for (int x = 0; x < M; x++) {
+                for (int y = 0; y < M; y++) {
+                    if (A[i + x][j + y] != B[x][y]) {
+                        match = false;
+                    }
+                }
+            }
+
+            if (match) {
+                flag = true;
+            }
+        }
+    }
+
+    if (flag) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-// ABC075A
+// ABC054B
 #include <bits/stdc++.h>
 #include <iostream>
 #include <cstring>
@@ -18,15 +18,48 @@ using namespace std;
 
 int main()
 {
-    int A, B, C;
-    cin >> A >> B >> C;
+    int N, M;
+    char A[50][50], B[50][50];
+    cin >> N >> M;
 
-    if (A == B) {
-        cout << C << endl;
-    } else if (B == C) {
-        cout << A << endl;
-    } else if (C == A) {
-        cout << B << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    bool flag = false;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if ((i + M -1 >= N) || (j + M - 1 >= N)) {
+                continue;
+            }
+
+            bool match = true;
+            for (int x = 0; x < M; x++) {
+                for (int y = 0; y < M; y++) {
+                    if (A[i + x][j + y] != B[x][y]) {
+                        match = false;
+                    }
+                }
+            }
+
+            if (match) {
+                flag = true;
+            }
+        }
+    }
+
+    if (flag) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
     }
 
     return 0;
