c28f68387b2dacdf20e18f39daf425b22009b08f
Ramune6110 <arduino6110@gmail.com>
2021-01-21 11:12:11 +0900

    [add]Problem 061B

--- AtCoder_ProblemB.cpp
+++ AtCoder_ProblemB.cpp
@@ -543,3 +543,44 @@ int main()
 
     return 0;
 }
+
+// ABC061B
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using namespace std;
+
+int main()
+{   
+    int N, M;
+    cin >> N >> M;
+
+    int a[M], b[M];
+    for (int i = 1; i <= M; i++) {
+        cin >> a[i - 1] >> b[i - 1];
+    }
+
+    int num[N] = {0};
+    for (int i = 1; i <= M; i++) {
+        num[a[i - 1] - 1]++;
+        num[b[i - 1] - 1]++;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        cout << num[i - 1] << endl;
+    }
+
+    return 0;
+}
