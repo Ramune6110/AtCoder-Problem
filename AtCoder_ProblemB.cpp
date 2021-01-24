2530f206b97a08e20210cee0f457616eaae4d526
Ramune6110 <arduino6110@gmail.com>
2021-01-24 22:44:37 +0900

    [add]Problem 056B

--- AtCoder_ProblemB.cpp
+++ AtCoder_ProblemB.cpp
@@ -487,6 +487,45 @@ int main()
     return 0;
 }
 
+// ABC056B
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using namespace std;
+
+int main() 
+{
+    int W, a, b;
+    cin >> W >> a >> b;
+
+    int A = a + W;
+    int B = b;
+    int C = a;
+    int D = b + W;
+
+    if (A < B) {
+        cout << B - A << endl;
+    } else if (C > D) {
+        cout << C -D << endl;
+    } else if ((A > B) || (C < D) || (A == B) || (C == D)) {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
+
 // ABC057B
 #include <bits/stdc++.h>
 #include <iostream>
--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-// ABC081B
+// ABC056B
 #include <bits/stdc++.h>
 #include <iostream>
 #include <cstring>
@@ -16,46 +16,24 @@
 
 using namespace std;
 
-bool divide_by_two(int A[], int N);
-
 int main() 
 {
-    int N;
-    cin >> N;
+    int W, a, b;
+    cin >> W >> a >> b;
 
-    int A[N];
-    for (int i = 1; i <= N; i++) {
-        cin >> A[i - 1];
+    int A = a + W;
+    int B = b;
+    int C = a;
+    int D = b + W;
+
+    if (A < B) {
+        cout << B - A << endl;
+    } else if (C > D) {
+        cout << C -D << endl;
+    } else if ((A > B) || (C < D) || (A == B) || (C == D)) {
+        cout << 0 << endl;
     }
-    
-    int cnt = 0;
-    bool flag = true;
-    while (flag) {
-        flag = divide_by_two(A, N);
-
-        for (int i = 1; i <= N; i++) {
-            A[i - 1] = A[i - 1] / 2;
-        }
-
-        cnt++;
-    }
-
-    cout << cnt - 1 << endl;
 
     return 0;
 }
 
-// 全ての数が2で割れるかを判定する関数
-bool divide_by_two(int A[], int N) {
-    bool flag = false;
-    for (int i = 1; i <= N; i++) {
-        if (A[i - 1] % 2 == 0) {
-            flag = true;
-        } else {
-            flag = false;
-            break;
-        }
-    }
-
-    return flag;
-}
