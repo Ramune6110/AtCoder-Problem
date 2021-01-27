24688e52b7105025286c32ccf05c8a134e705701
Ramune6110 <arduino6110@gmail.com>
2021-01-27 09:51:11 +0900

    [add]Problem A

--- AtCoder_ProblemA.cpp
+++ AtCoder_ProblemA.cpp
@@ -1358,3 +1358,69 @@ bool digsum(int num)
 
     return flag;
 }
+
+// ABC080A
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using namespace std;
+
+int main()
+{
+    int N, A, B;
+    cin >> N >> A >> B;
+
+    if ((A * N) > B) {
+        cout << B << endl;
+    } else if ((A * N) < B) {
+        cout << A * N << endl;
+    } else if ((A * N) == B) {
+        cout << B << endl;
+    }
+
+    return 0;
+}
+
+// ABC081A
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    double ans;
+    // 四捨五入 : https://daeudaeu.com/kurisuke_kiriage_shisyagonyu/#i-5
+    ans = round(((double)a + (double)b) / 2);
+
+    cout << ans << endl;
+
+    return 0;
+}
--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-// ABC079A
+// ABC081A
 #include <bits/stdc++.h>
 #include <iostream>
 #include <cstring>
@@ -16,45 +16,16 @@
 
 using namespace std;
 
-bool digsum(int num);
-
 int main()
 {
-    int N;
-    bool flag;
-    
-    cin >> N;
-    
-    flag = digsum(N);
+    int a, b;
+    cin >> a >> b;
 
-    if (flag == true) {
-        cout << "Yes" << endl;
-    } else if (flag == false) {
-        cout << "No" << endl;
-    }
+    double ans;
+    // 四捨五入 : https://daeudaeu.com/kurisuke_kiriage_shisyagonyu/#i-5
+    ans = round(((double)a + (double)b) / 2);
+
+    cout << ans << endl;
 
     return 0;
-}
-
-// 整数の各桁の数字が等しいかを判定する関数
-bool digsum(int num) 
-{
-    int dig[4];
-    for (int i = 1; i <= 4; i++) {
-        dig[i - 1] = num % 10;
-        num = num / 10;
-    }
-
-    bool flag = false;
-    if ((dig[0] == dig[1]) && (dig[1] == dig[2])) {
-        flag = true;
-    } else if ((dig[1] == dig[2]) && (dig[2] == dig[3])) {
-        flag = true;
-    } else if ((dig[0] == dig[1]) && (dig[1] == dig[2]) && (dig[2] == dig[3]) && (dig[3] == dig[0])) {
-        flag = true;
-    } else {
-        flag = false;
-    }
-
-    return flag;
 }
