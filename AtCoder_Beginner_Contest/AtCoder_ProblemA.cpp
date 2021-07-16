f9ad931b9c6a46bd6db37a947bc5ea8ae8a98a6c
Ramune6110 <arduino6110@gmail.com>
2021-07-16 10:47:34 +0900

    [add]083A

--- AtCoder_Beginner_Contest/AtCoder_ProblemA.cpp
+++ AtCoder_Beginner_Contest/AtCoder_ProblemA.cpp
@@ -1457,6 +1457,45 @@ int main()
     return 0;
 }
 
+// ABC083A
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using ll = long long;
+using namespace std;
+
+int main()
+{   
+    int A, B, C, D;
+	cin >> A >> B >> C >> D;
+
+	int left_weight = A + B;
+	int right_weight = C + D;
+
+	if (left_weight > right_weight) {
+		cout << "Left" << endl;
+	} else if (left_weight == right_weight) {
+		cout << "Balanced" << endl;
+	} else if (left_weight < right_weight) {
+		cout << "Right" << endl;
+	}
+
+    return 0;
+}
+
 // ABC190A
 #include <bits/stdc++.h>
 #include <iostream>
--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-// ABC191D
+// ABC082A
 #include <bits/stdc++.h>
 #include <iostream>
 #include <cstring>
@@ -13,45 +13,26 @@
 #include <set>
 
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-#define FOR(i, n, m) for(int i = (int)(n); i < (int)(m); i++)
 #define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
 
 using ll = long long;
 using namespace std;
 
-ll in(){
-	double x;
-    cin >> x;
-	x*=10000;
-	return round(x);
-}
- 
-bool ok(ll x,ll y,ll r){
-	return x*x + y*y <= r*r;
-}
- 
-ll num(ll x,ll y,ll z,ll lim){
-	int l = 0;
-	int r = 1;
-	ll res = 0;
-	for(int i = int(1e9)+50000;i >= lim;i -= 10000){
-		while(ok(x-l*10000,i-y,z)) --l;
-		while(ok(r*10000-x,i-y,z)) ++r;
-		res += r-l-1;
+int main()
+{   
+    int A, B, C, D;
+	cin >> A >> B >> C >> D;
+
+	int left_weight = A + B;
+	int right_weight = C + D;
+
+	if (left_weight > right_weight) {
+		cout << "Left" << endl;
+	} else if (left_weight == right_weight) {
+		cout << "Balanced" << endl;
+	} else if (left_weight < right_weight) {
+		cout << "Right" << endl;
 	}
-	return res;
-}
- 
-int main(){
-	ll x = in();
-	ll y = in();
-	ll z = in();
-	x %= 10000;
-	y %= 10000;
-	ll ans = 0;
-	ans += num(x,y,z,10000);
-	ans += num(x,-y,z,0);
-	cout << ans << endl;
 
     return 0;
 }
