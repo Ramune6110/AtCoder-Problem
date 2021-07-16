c182359eea23af06e5fc359b68e9118cce4e047a
Ramune6110 <arduino6110@gmail.com>
2021-07-16 11:00:24 +0900

    [add]102B

--- AtCoder_Beginner_Contest/AtCoder_ProblemB.cpp
+++ AtCoder_Beginner_Contest/AtCoder_ProblemB.cpp
@@ -1736,6 +1736,48 @@ int main()
     return 0;
 } 
 
+// ABC102B
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using ll = long long;
+using namespace std;
+
+long int digsum(long int num);
+
+int main()
+{   
+    int N;
+	cin >> N;
+
+	vector<ll> A(N);
+	for(int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	// vector要素の中の最大値と最小値を抽出する関数
+	// https://shunichiro.net/2019/10/01/c-vector%E3%81%8B%E3%82%89%E6%9C%80%E5%A4%A7%E5%80%A4%E3%83%BB%E6%9C%80%E5%B0%8F%E5%80%A4%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B/
+    ll max = *max_element(A.begin(), A.end());
+    ll min = *min_element(A.begin(), A.end());
+    
+	cout << abs(max - min) << endl;
+
+	return 0;
+}
+
 // ABC105B
 #include <bits/stdc++.h>
 #include <iostream>
--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-// ABC084A
+// ABC102B
 #include <bits/stdc++.h>
 #include <iostream>
 #include <cstring>
@@ -18,12 +18,24 @@
 using ll = long long;
 using namespace std;
 
+long int digsum(long int num);
+
 int main()
 {   
-    int M;
-	cin >> M;
+    int N;
+	cin >> N;
 
-	cout << (24 - M) + 24 << endl;
+	vector<ll> A(N);
+	for(int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	// vector要素の中の最大値と最小値を抽出する関数
+	// https://shunichiro.net/2019/10/01/c-vector%E3%81%8B%E3%82%89%E6%9C%80%E5%A4%A7%E5%80%A4%E3%83%BB%E6%9C%80%E5%B0%8F%E5%80%A4%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B/
+    ll max = *max_element(A.begin(), A.end());
+    ll min = *min_element(A.begin(), A.end());
+    
+	cout << abs(max - min) << endl;
 
 	return 0;
 }
