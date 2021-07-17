480efc28f4b1445da7313d0049a4fe3c037d8f9b
Ramune6110 <arduino6110@gmail.com>
2021-07-17 12:00:50 +0900

    [add]115B

--- AtCoder_Beginner_Contest/AtCoder_ProblemB.cpp
+++ AtCoder_Beginner_Contest/AtCoder_ProblemB.cpp
@@ -2084,6 +2084,62 @@ int main()
 	return 0;
 } 
 
+// ABC115B
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using ll = long long;
+using namespace std;
+
+long int digsum(long int num);
+
+int main()
+{   
+	int N;
+	cin >> N;
+
+	vector<int> p(N);
+	for (int i = 0; i < N; i++) {
+		cin >> p[i];
+	}
+
+	// vector要素の中の最大値と最小値を抽出する関数
+	// https://shunichiro.net/2019/10/01/c-vector%E3%81%8B%E3%82%89%E6%9C%80%E5%A4%A7%E5%80%A4%E3%83%BB%E6%9C%80%E5%B0%8F%E5%80%A4%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B/
+    int max = *max_element(p.begin(), p.end());
+
+    // vextor内の特定の要素のindexを算出
+	// https://www.cns.s.u-tokyo.ac.jp/~masuoka/post/search_vector_index/
+	vector<int>::iterator itr;
+    itr = find(p.begin(), p.end(), max);
+    const int index = distance(p.begin(), itr);
+    
+	// 合計金額の算出
+	int totalcost = 0;
+	for (int i = 0; i < N; i++) {
+		if (i == index) {
+			p[i] = p[i] / 2;
+		}
+		totalcost += p[i];
+	}
+
+	cout << totalcost << endl;
+
+	return 0;
+} 
+
 // ABC123B
 #include <bits/stdc++.h>
 #include <iostream>
--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-// ABC114B
+// ABC115B
 #include <bits/stdc++.h>
 #include <iostream>
 #include <cstring>
@@ -22,26 +22,34 @@ long int digsum(long int num);
 
 int main()
 {   
-    string S;
-	cin >> S;
-    
-	int minnum = 10000;
-	for (int i = 0; i < S.length() - 2; i++) {
-		string sub = S.substr(i, 3);
-		// 文字列から数字型へ変換
-		// https://qiita.com/MasahiroBW/items/3f56b22a079cd3272cd3
-		int num;
-		istringstream number;
+	int N;
+	cin >> N;
 
-		number = istringstream(sub);
-		number >> num;
-
-		if (abs(num - 753) < minnum ) {
-			minnum = abs(num - 753);
-		}
+	vector<int> p(N);
+	for (int i = 0; i < N; i++) {
+		cin >> p[i];
 	}
 
-	cout << minnum << endl;
+	// vector要素の中の最大値と最小値を抽出する関数
+	// https://shunichiro.net/2019/10/01/c-vector%E3%81%8B%E3%82%89%E6%9C%80%E5%A4%A7%E5%80%A4%E3%83%BB%E6%9C%80%E5%B0%8F%E5%80%A4%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B/
+    int max = *max_element(p.begin(), p.end());
+
+    // vextor内の特定の要素のindexを算出
+	// https://www.cns.s.u-tokyo.ac.jp/~masuoka/post/search_vector_index/
+	vector<int>::iterator itr;
+    itr = find(p.begin(), p.end(), max);
+    const int index = distance(p.begin(), itr);
+    
+	// 合計金額の算出
+	int totalcost = 0;
+	for (int i = 0; i < N; i++) {
+		if (i == index) {
+			p[i] = p[i] / 2;
+		}
+		totalcost += p[i];
+	}
+
+	cout << totalcost << endl;
 
 	return 0;
 } 
