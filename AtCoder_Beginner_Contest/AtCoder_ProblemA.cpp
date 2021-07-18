c34e8fdfff4ce16f9e1debf95c188d578c984cbf
Ramune6110 <arduino6110@gmail.com>
2021-07-18 11:50:38 +0900

    [add]087A

--- AtCoder_Beginner_Contest/AtCoder_ProblemA.cpp
+++ AtCoder_Beginner_Contest/AtCoder_ProblemA.cpp
@@ -1597,6 +1597,47 @@ int main()
 	return 0;
 } 
 
+// ABC087A
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using ll = long long;
+using namespace std;
+
+long int digsum(long int num);
+
+int main()
+{   
+	int X, A, B;
+	cin >> X >> A >> B;
+
+	int total = X - A;
+	while(total > 0) {
+		total -= B;
+		if (total < 0) {
+			total = total + B;
+			break;
+		}
+	}
+
+	cout << total << endl;
+
+	return 0;
+} 
+
 // ABC190A
 #include <bits/stdc++.h>
 #include <iostream>
