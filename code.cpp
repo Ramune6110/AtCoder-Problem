49cae0c022085e89b29e0d5c85cab771c8755210
Ramune6110 <arduino6110@gmail.com>
2021-11-28 16:12:32 +0900

    [add]106A

--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-// ABC173B
+// ABC106A
 #include <bits/stdc++.h>
 #include <iostream>
 #include <cstring>
@@ -20,34 +20,11 @@ using namespace std;
 
 int main()
 {
-    ll N;
-    cin >> N;
+    int A, B;
+    cin >> A >> B;
 
-    vector<string> S(N);
-    for (ll i = 0; i < N; i++) {
-        cin >> S[i];
-    }
-
-    ll C0 = 0;
-    ll C1 = 0;
-    ll C2 = 0;
-    ll C3 = 0;
-    for (ll i = 0; i < N; i++) {
-        if (S[i] == "AC") {
-            C0++;
-        } else if (S[i] == "WA") {
-            C1++;
-        } else if (S[i] == "TLE") {
-            C2++;
-        } else if (S[i] == "RE") {
-            C3++;
-        }
-    }
-
-    cout << "AC" << " " << "x" << " " << C0 << endl;
-    cout << "WA" << " " << "x" << " " << C1 << endl;
-    cout << "TLE" << " " << "x" << " " << C2 << endl;
-    cout << "RE" << " " << "x" << " " << C3 << endl;
+    int yard = (A - 1) * (B - 1);
+    cout << yard << endl;
 
     return 0;
 }
