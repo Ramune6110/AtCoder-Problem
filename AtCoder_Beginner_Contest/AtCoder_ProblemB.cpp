c8cb7ee36658617b66f63cd4ae36b28e79c834fa
Ramune6110 <arduino6110@gmail.com>
2021-12-25 11:03:34 +0900

    [add]216B

--- AtCoder_Beginner_Contest/AtCoder_ProblemB.cpp
+++ AtCoder_Beginner_Contest/AtCoder_ProblemB.cpp
@@ -5213,3 +5213,52 @@ int main()
     
     return 0;
 }
+
+// ABC216B
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <regex>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <set>
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+using ll = long long;
+using namespace std;
+
+int main()
+{   
+    int N;
+    cin >> N;
+
+    vector<string> S(N);
+    vector<string> T(N);
+    for (int i = 0; i < N; i++) {
+        cin >> S[i] >> T[i];
+    }
+ 
+    bool flag = false;
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if (S[i] == S[j] && T[i] == T[j]) {
+                flag = true;
+            }
+        }
+    }
+
+    if (flag == true) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-// ABC213B
+// ABC216B
 #include <bits/stdc++.h>
 #include <iostream>
 #include <cstring>
@@ -23,31 +23,26 @@ int main()
     int N;
     cin >> N;
 
-    vector<int> A(N);
-    vector<int> dammy_A(N);
+    vector<string> S(N);
+    vector<string> T(N);
     for (int i = 0; i < N; i++) {
-        cin >> A[i];
-        dammy_A[i] = A[i];
+        cin >> S[i] >> T[i];
+    }
+ 
+    bool flag = false;
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if (S[i] == S[j] && T[i] == T[j]) {
+                flag = true;
+            }
+        }
     }
 
-    //降順ソート
-    // http://7ujm.net/stl/sort.html
-    sort(A.begin(),A.end(),std::greater<int>());
-    
-    // 特定の要素のindexを取得
-    // https://www.cns.s.u-tokyo.ac.jp/~masuoka/post/search_vector_index/
-    std::vector<int>::iterator itr;
-
-    // A[1]に対応するindexを取って来たい
-    const int wanted = A[1]; 
-    itr = std::find(dammy_A.begin(), dammy_A.end(), wanted);
-    if (itr == dammy_A.end()) {
-        std::cout << "search failed" << std::endl;
+    if (flag == true) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
     }
-    
-    const int wanted_index = distance(dammy_A.begin(), itr);
 
-    cout << wanted_index + 1 << endl;
-    
     return 0;
 }
